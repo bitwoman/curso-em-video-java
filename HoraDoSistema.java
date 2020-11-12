27b0193b595289be9ab1e0d8912af5333b801868
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-11-12 11:19:59 -0300

    HoraDoSistema

---
+++ HoraDoSistema.java
@@ -0,0 +1,12 @@
+package CursoEmVideo;
+
+import java.util.Date;
+
+public class HoraDoSistema {
+
+	public static void main(String[] args) {
+		Date watch = new Date();
+		
+		System.out.println("TIMESTAMP: " + watch.toString());
+	}
+}
