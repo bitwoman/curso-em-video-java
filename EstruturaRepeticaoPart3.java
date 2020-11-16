3c9bc32dad87b1f49e897c19b1cd7fb78ecc1656
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-11-16 11:24:20 -0300

    EstruturaRepeticaoPart3

---
+++ EstruturaRepeticaoPart3.java
@@ -0,0 +1,10 @@
+package cursoemvideo;
+
+public class EstruturaRepeticaoPart3 {
+
+	public static void main(String[] args) {
+		for(int cc=0; cc<4; cc++) {
+			System.out.println("Cambalhota: " + cc);
+		}
+	}
+}
