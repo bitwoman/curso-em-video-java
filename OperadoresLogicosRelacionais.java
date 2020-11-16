141b2bd01f77c1cd7699f621d548f9aa5c72025b
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-11-16 11:26:39 -0300

    OperadoresLogicosRelacionais

---
+++ OperadoresLogicosRelacionais.java
@@ -0,0 +1,16 @@
+package cursoemvideo;
+
+public class OperadoresLogicosRelacionais {
+
+	public static void main(String[] args) {
+		
+		int numeroUm, numeroDois, resultado;
+		numeroUm = 4;
+		numeroDois = 8;
+		
+//		Operador ternário:
+		resultado = (numeroUm>numeroDois)?0:1;
+		
+		System.out.println(resultado);
+	}
+}
