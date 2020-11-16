84b90fc7dba71e9abe08bd808b6eb4bafd12694c
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-11-16 11:27:14 -0300

    TiposPrimitivos

---
+++ TiposPrimitivos.java
@@ -0,0 +1,14 @@
+package cursoemvideo;
+
+public class TiposPrimitivos {
+
+	public static void main(String[] args) {
+//		new -> objeto. Wrapper Class -> Invólucro.
+		float nota = 8.5f;
+			
+		System.out.print("A sua nota é " + nota);
+		System.out.println("A sua nota é " + nota);
+		System.out.printf("A sua nota é %.2f", nota);
+		System.out.format("A sua nota é %.2f", nota);
+	}
+}
